404da7f63a0db8e117d22d6d6ccf82a7c430986f
ZiyuLuNeu <113486280+ZiyuLuNeu@users.noreply.github.com>
2022-09-22 22:12:22 -0400

    Add files via upload
    homework01

---
+++ src/model/User.java
@@ -0,0 +1,199 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author ziyu
+ */
+public class User {
+    
+private String FirstName;
+private String LastName;
+private String DOB;
+private String Age;
+private String TelephoneNumber;
+private String Email;
+private String StreetLine1;
+private String StreetLine2;
+private String City;
+private String Country;
+private String Affiliation;
+private String Major;
+private String Career;
+private String Degree1;
+private String Degree1Start;
+private String Degree1End;
+private String Degree2;
+private String Degree2Start;
+private String Degree2End;
+private String photo;
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String FirstName) {
+        this.FirstName = FirstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String LastName) {
+        this.LastName = LastName;
+    }
+
+    public String getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(String DOB) {
+        this.DOB = DOB;
+    }
+
+    public String getAge() {
+        return Age;
+    }
+
+    public void setAge(String Age) {
+        this.Age = Age;
+    }
+
+    public String getTelephoneNumber() {
+        return TelephoneNumber;
+    }
+
+    public void setTelephoneNumber(String TelephoneNumber) {
+        this.TelephoneNumber = TelephoneNumber;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public String getStreetLine1() {
+        return StreetLine1;
+    }
+
+    public void setStreetLine1(String StreetLine1) {
+        this.StreetLine1 = StreetLine1;
+    }
+
+    public String getStreetLine2() {
+        return StreetLine2;
+    }
+
+    public void setStreetLine2(String StreetLine2) {
+        this.StreetLine2 = StreetLine2;
+    }
+
+    public String getCity() {
+        return City;
+    }
+
+    public void setCity(String City) {
+        this.City = City;
+    }
+
+    public String getCountry() {
+        return Country;
+    }
+
+    public void setCountry(String Country) {
+        this.Country = Country;
+    }
+
+    public String getAffiliation() {
+        return Affiliation;
+    }
+
+    public void setAffiliation(String Affiliation) {
+        this.Affiliation = Affiliation;
+    }
+
+    public String getMajor() {
+        return Major;
+    }
+
+    public void setMajor(String Major) {
+        this.Major = Major;
+    }
+
+    public String getCareer() {
+        return Career;
+    }
+
+    public void setCareer(String Career) {
+        this.Career = Career;
+    }
+
+    public String getDegree1() {
+        return Degree1;
+    }
+
+    public void setDegree1(String Degree1) {
+        this.Degree1 = Degree1;
+    }
+
+    public String getDegree1Start() {
+        return Degree1Start;
+    }
+
+    public void setDegree1Start(String Degree1Start) {
+        this.Degree1Start = Degree1Start;
+    }
+
+    public String getDegree1End() {
+        return Degree1End;
+    }
+
+    public void setDegree1End(String Degree1End) {
+        this.Degree1End = Degree1End;
+    }
+
+    public String getDegree2() {
+        return Degree2;
+    }
+
+    public void setDegree2(String Degree2) {
+        this.Degree2 = Degree2;
+    }
+
+    public String getDegree2Start() {
+        return Degree2Start;
+    }
+
+    public void setDegree2Start(String Degree2Start) {
+        this.Degree2Start = Degree2Start;
+    }
+
+    public String getDegree2End() {
+        return Degree2End;
+    }
+
+    public void setDegree2End(String Degree2End) {
+        this.Degree2End = Degree2End;
+    }
+
+    
+    
+
+    
+}
